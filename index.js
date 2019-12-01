d2f0ff65b252508986a1e7bc89411d85c7db43b5
Kwanghyuk Kim <kimkw@oregonstate.edu>
2019-11-30 23:31:40 -0800

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,93 @@
         }
       }
 /* Item Listing */
+
+
+
+ /* start add-recipe-button */
+var pressPlusButton = document.querySelector('#add-recipe-button');
+pressPlusButton.addEventListener('click', function() {
+	document.getElementById("modal-add-recipe-backdrop").classList.remove('hidden');
+	document.getElementById("add-recipe-modal").classList.remove('hidden');
+	}
+);
+
+var closeButton = document.querySelector('#modal-add-recipe-close');
+closeButton.addEventListener('click', closeModal);
+
+var cancelButton = document.querySelector('#modal-cancel');
+cancelButton.addEventListener('click', closeModal);
+
+var addButton = document.querySelector('#modal-add');
+addButton.addEventListener('click', addRecipe);
+
+function closeModal() {
+	document.getElementById("recipe-name-input").value = "";
+	document.getElementById("recipe-photo-input").value = "";
+	document.getElementById("recipe-categories-input").value = "";
+	document.getElementById("modal-add-recipe-backdrop").classList.add('hidden');
+	document.getElementById("add-recipe-modal").classList.add('hidden');
+}
+
+function addRecipe() {
+	var recipe = document.createElement('div');
+	recipe.classList.add('recipe');
+
+	var recipeNameInput = document.getElementById("recipe-name-input").value;
+	var recipePhotoInput = document.getElementById("recipe-photo-input").value;
+	var recipeCategoriesInput = document.getElementById("recipe-categories-input").value;
+	recipe.setAttribute("data-city", recipeCategoriesInput);
+
+	var recipeContents = document.createElement('div');
+	recipeContents.classList.add('post-contents');
+	recipe.appendChild(recipeContents);
+
+	var recipeImageContainer = document.createElement('div');
+	recipeImageContainer.classList.add('post-image-container');
+	recipeContents.appendChild(recipeImageContainer);
+
+	var img = document.createElement('img');
+	img.setAttribute("src", recipePhotoInput);
+	img.setAttribute("alt", recipeNameInput);
+	recipeImageContainer.appendChild(img);
+
+
+	var recipeInfoContainer = document.createElement('div');
+	recipeInfoContainer.classList.add('post-info-container');
+	recipeContents.appendChild(recipeInfoContainer);
+
+	var recipeName = document.createElement('a');
+	recipeName.setAttribute("href", "#");
+	recipeName.classList.add('post-title');
+	recipeName.textContent = recipeNameInput;
+	recipeInfoContainer.appendChild(recipeName);
+
+	if(isEmpty()){
+		document.getElementById('items').appendChild(recipe);
+		addCategories(recipeCategoriesInput);
+		postData.push(recipe);
+		closeModal();
+	}
+}
+
+function isEmpty() {
+	var addInfo = document.querySelectorAll('.modal-add-recipe-body input');
+	for (var i = 0; i < 3; i++) {
+		if (addInfo[i].value == "") {
+			alert("Please fill out all blanks to add an item.");
+			return false;
+		}
+	}
+	return true;
+}
+
+function addCategories(categories) {
+	var filterCategories = document.getElementById("filter-categories");
+	var numCategories = filterCategories.length;
+	for (var i = 0; i < numCategories; i++) {
+		if ((i + 1) == numCategories) {
+			filterCategories.options.add(new Option(categories, categories));
+		}
+	}
+}
+ /* End add-recipe-button */
