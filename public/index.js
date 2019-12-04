e1e4d343e1adfd1e5990f1fe4c0ed97c776bdca1
shinhoj01 <56090472+shinhoj01@users.noreply.github.com>
2019-12-03 22:27:46 -0800

    Handlebars/gitignore/minor tweaks

--- public/index.js
+++ public/index.js
@@ -1,25 +1,25 @@
 /*
  * Write JS code in this file.
  */
- function searchbox_filter(){
+function searchbox_filter(){
 
-        var value, name, item, i;
+	var value, name, item, i;
 
-        value = document.getElementById("value").value.toUpperCase();
-        items = document.getElementsByClassName("item");
+	value = document.getElementById("value").value.toUpperCase();
+	items = document.getElementsByClassName("item");
 
-        for(i=0;i<items.length;i++){
-          title = items[i].getElementsByClassName("item-title");
-          if(title[0].innerHTML.toUpperCase().indexOf(value) > -1){
-            items[i].style.display = "inline-block";
+	for(i=0;i<items.length;i++){
+		title = items[i].getElementsByClassName("item-title");
+		if(title[0].innerHTML.toUpperCase().indexOf(value) > -1){
+			items[i].style.display = "inline-block";
 
-            // Items pictures are will show up here..
-          }else{
-            items[i].style.display = "none";
-          }
-        }
+		// Items pictures are will show up here..
+		}else{
+			items[i].style.display = "none";
+		}
+	}
 
-      }
+}
 /* Item Listing */
 
 
