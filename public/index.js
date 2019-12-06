2b18275e53c53bdedda719186a094402baedb0c5
shinhoj01 <56090472+shinhoj01@users.noreply.github.com>
2019-12-05 21:14:02 -0800

    Added handlebars runtime

--- public/index.js
+++ public/index.js
@@ -55,37 +55,25 @@ function addRecipe() {
 	var img_url = document.getElementById("recipe-photo-input").value;
 	var caption = document.getElementById("recipe-name-input").value;
 
-	var itemRecipeHTML = Handlebars.templates.item({
-		BOOKMARK: bookmark,
-		CATEGORIES: categories,
-		INGREDIENTS: ingredients,
-		IMG_URL: img_url,
-		CAPTION: caption
-	});
+	if(!categories || !img_url || !caption){
+		alert("Please fill out all blanks to add an item");
+		
+	}else{
+		var itemRecipe = {
+			BOOKMARK: bookmark,
+			CATEGORIES: categories,
+			INGREDIENTS: ingredients,
+			IMG_URL: img_url,
+			CAPTION: caption
+		};
 
-	if(isEmpty()){
+		var itemRecipeHTML = Handlebars.templates.item(itemRecipe);
+
 		var itemsSection = document.getElementById('items');
 		itemsSection.insertAdjacentHTML('beforeend', itemRecipeHTML);
 		closeModal();
 	}
 }
-
-
-/*ERROR*/
-function isEmpty() {
-	var addInfo = document.querySelectorAll('.modal-add-recipe-body input');
-
-
-  for (var i = 0; i < 3; i++) {
-		if (addInfo[i].value == "") {
-			alert("Please fill out all blanks to add an item.");
-			return false;
-		}
-	}
-    return true;
-}
-/*WORKING*/
-
 
 function addCategories(categories) {
 	var filterCategories = document.getElementById("filter-categories");
