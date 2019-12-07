88eeb7a03191b0a3b5c7ef5b8dc7df555efa2be1
Hongyiel <suhho@oregonstate.edu>
2019-12-07 11:28:17 -0800

    fix

--- public/index.js
+++ public/index.js
@@ -3,84 +3,69 @@
 /*
  * Write JS code in this file.
  */
-// <<<<<<< Hong
-// function searchbox_filter() {
-
-//   var value, name, item, i;
-
-//   value = document.getElementById("value").value.toUpperCase();
-//   items = document.getElementsByClassName("item");
-
-//   for (i = 0; i < items.length; i++) {
-//     title = items[i].getElementsByClassName("item-title");
-//     if (title[0].innerHTML.toUpperCase().indexOf(value) > -1) {
-//       items[i].style.display = "inline-block";
-
-//       // Items pictures are will show up here..
-//     } else {
-//       items[i].style.display = "none";
-//     }
-//   }
-// =======
 
 var globalFavoriteFlag = false;
-function searchbox_filter(){
-	
-	var filterInput = document.getElementById("filter-categories").value;
-	var value, cgFlag, bkFlag, items, i;
 
-	value = document.getElementById("value").value.toUpperCase();
-	items = document.getElementsByClassName('item');
+function searchbox_filter() {
 
-	for(i=0;i<items.length;i++){
-		title = items[i].getElementsByClassName("item-title");
-		bkFlag = items[i].dataset.bookmark;
-		if(filterInput == 'Any'){
-			cgFlag = 'Any';
-		}else{
-			cgFlag = items[i].dataset.categories;
-		}
+  var filterInput = document.getElementById("filter-categories").value;
+  var value, cgFlag, bkFlag, items, i;
 
-		//console.log('== cgFlag:', cgFlag);
+  value = document.getElementById("value").value.toUpperCase();
+  items = document.getElementsByClassName('item');
 
-		if(globalFavoriteFlag){
-			if(title[0].innerHTML.toUpperCase().indexOf(value) > -1 && bkFlag == 'true' && filterInput == cgFlag){
-				items[i].style.display = "inline-block";
+  for (i = 0; i < items.length; i++) {
+    title = items[i].getElementsByClassName("item-title");
+    bkFlag = items[i].dataset.bookmark;
+    if (filterInput == 'Any') {
+      cgFlag = 'Any';
+    } else {
+      cgFlag = items[i].dataset.categories;
+    }
 
-			}else{
-				items[i].style.display = "none";
-			}
-		}else{
-			if(title[0].innerHTML.toUpperCase().indexOf(value) > -1 && filterInput == cgFlag){
-				items[i].style.display = "inline-block";
+    //console.log('== cgFlag:', cgFlag);
 
-			}else{
-				items[i].style.display = "none";
-			}
-		}
-		
-	}
+    if (globalFavoriteFlag) {
+      if (title[0].innerHTML.toUpperCase().indexOf(value) > -1 && bkFlag == 'true' && filterInput == cgFlag) {
+        items[i].style.display = "inline-block";
+
+      } else {
+        items[i].style.display = "none";
+      }
+    } else {
+      if (title[0].innerHTML.toUpperCase().indexOf(value) > -1 && filterInput == cgFlag) {
+        items[i].style.display = "inline-block";
+
+      } else {
+        items[i].style.display = "none";
+      }
+    }
+
+  }
 
 }
 
 $('#bookmark-filter-button').on('click', function() {
-	if($(this).attr('class')==="fa-star far"){
-		$(this).removeClass('far');
-		$(this).addClass('fas');
-		globalFavoriteFlag = true;
-		searchbox_filter();
-	}else{
-		globalFavoriteFlag = false;
-		$(this).removeClass('fas');
-		$(this).addClass('far');
-		searchbox_filter();
-	}
+  if ($(this).attr('class') === "fa-star far") {
+    $(this).removeClass('far');
+    $(this).addClass('fas');
+    globalFavoriteFlag = true;
+    searchbox_filter();
+  } else {
+    globalFavoriteFlag = false;
+    $(this).removeClass('fas');
+    $(this).addClass('far');
+    searchbox_filter();
+  }
 });
 $('#filter-categories').on('click', function() {
-	searchbox_filter();
-  });
+  searchbox_filter();
+});
 
-// Create a "close" button and append it to each list item
+
+
+
+// Creating array
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -140,10 +125,14 @@ function newElement() {
 
 
 
- /* start add-recipe-button */
+
+
+
+
+/* start add-recipe-button */
 $('#add-recipe-button').on('click', function() {
-	$('#modal-add-recipe-backdrop').removeClass('hidden');
-	$('#add-recipe-modal').removeClass('hidden');
+  $('#modal-add-recipe-backdrop').removeClass('hidden');
+  $('#add-recipe-modal').removeClass('hidden');
 });
 
 $('#modal-add-recipe-close').on('click', closeModal);
@@ -152,63 +141,63 @@ $('#modal-add').on('click', addRecipe);
 
 function closeModal() {
 
-	$('#recipe-name-input').val("");
-	$('#recipe-photo-input').val("");
-	$('recipe-categories-input').val("");
-	$('#modal-add-recipe-backdrop').addClass('hidden');
-	$('#add-recipe-modal').addClass('hidden');
+  $('#recipe-name-input').val("");
+  $('#recipe-photo-input').val("");
+  $('recipe-categories-input').val("");
+  $('#modal-add-recipe-backdrop').addClass('hidden');
+  $('#add-recipe-modal').addClass('hidden');
 }
 
 function addRecipe() {
-	var bookmark = false;
-	var categories = $('#recipe-categories-input').val();
-	var ingredients = "placeholder";
-	var img_url = $('#recipe-photo-input').val();
-	var caption = $('#recipe-name-input').val();
+  var bookmark = false;
+  var categories = $('#recipe-categories-input').val();
+  var ingredients = "placeholder";
+  var img_url = $('#recipe-photo-input').val();
+  var caption = $('#recipe-name-input').val();
 
-	if(!categories || !img_url || !caption){
-		alert("Please fill out all blanks to add an item");
+  if (!categories || !img_url || !caption) {
+    alert("Please fill out all blanks to add an item");
 
-	}else{
+  } else {
 
-		var postRequest = new XMLHttpRequest();
-		var requestURL = '/addItem';
-		postRequest.open('POST', requestURL);
+    var postRequest = new XMLHttpRequest();
+    var requestURL = '/addItem';
+    postRequest.open('POST', requestURL);
 
-		var requestBody = JSON.stringify({
-			BOOKMARK: bookmark,
-			CATEGORIES: categories,
-			INGREDIENTS: ingredients,
-			IMG_URL: img_url,
-			CAPTION: caption
-		});
+    var requestBody = JSON.stringify({
+      BOOKMARK: bookmark,
+      CATEGORIES: categories,
+      INGREDIENTS: ingredients,
+      IMG_URL: img_url,
+      CAPTION: caption
+    });
 
-		console.log("== Request Body:", requestBody);
-		postRequest.setRequestHeader('Content-Type', 'application/json');
+    console.log("== Request Body:", requestBody);
+    postRequest.setRequestHeader('Content-Type', 'application/json');
 
-		postRequest.addEventListener('load', function (event) {
-			console.log("== status:", event.target.status);
-			if(event.target.status !== 200){
-				var responseBody = event.target.response;
-				alert("Error saving item on server side: ", + responseBody);
-			} else {
-				var itemTemplate = Handlebars.templates.item;
-				var itemRecipeHTML = itemTemplate({
-					BOOKMARK: bookmark,
-					CATEGORIES: categories,
-					INGREDIENTS: ingredients,
-					IMG_URL: img_url,
-					CAPTION: caption
-				});
+    postRequest.addEventListener('load', function(event) {
+      console.log("== status:", event.target.status);
+      if (event.target.status !== 200) {
+        var responseBody = event.target.response;
+        alert("Error saving item on server side: ", +responseBody);
+      } else {
+        var itemTemplate = Handlebars.templates.item;
+        var itemRecipeHTML = itemTemplate({
+          BOOKMARK: bookmark,
+          CATEGORIES: categories,
+          INGREDIENTS: ingredients,
+          IMG_URL: img_url,
+          CAPTION: caption
+        });
 
-				var itemsSection = document.getElementById('items');
-				itemsSection.insertAdjacentHTML('beforeend', itemRecipeHTML);
-			}
-		});
+        var itemsSection = document.getElementById('items');
+        itemsSection.insertAdjacentHTML('beforeend', itemRecipeHTML);
+      }
+    });
 
-		postRequest.send(requestBody);
-		closeModal();
-	}
+    postRequest.send(requestBody);
+    closeModal();
+  }
 }
 
 function addCategories(categories) {
@@ -227,85 +216,84 @@ function addCategories(categories) {
 
 
 
- /* Misc Buttons */
- /* work in progress */
+/* Misc Buttons */
+/* work in progress */
 
 $('.item').each(function(index) {
-	if($(this).attr('data-bookmark')==true){
-		var longShitElem = $(this).children('.item-contents').children('.item-button-container').children('#bookmark');
-		longShitElem.removeClass('far');
-		longShitElem.addClass('fas');
-	}
+  if ($(this).attr('data-bookmark') == true) {
+    var longShitElem = $(this).children('.item-contents').children('.item-button-container').children('#bookmark');
+    longShitElem.removeClass('far');
+    longShitElem.addClass('fas');
+  }
 });
 
 $('section').on('click', '#trash', function() {
 
-	var itemElem = $(this).parent().parent().parent();
-	var caption = itemElem.attr('data-caption');
-	var postRequest = new XMLHttpRequest();
-	var requestURL = '/deleteItem';
-	postRequest.open('POST', requestURL);
+  var itemElem = $(this).parent().parent().parent();
+  var caption = itemElem.attr('data-caption');
+  var postRequest = new XMLHttpRequest();
+  var requestURL = '/deleteItem';
+  postRequest.open('POST', requestURL);
 
-	var requestBody = JSON.stringify({
-		CAPTION: caption
-	});
+  var requestBody = JSON.stringify({
+    CAPTION: caption
+  });
 
-	console.log("== Request Body:", requestBody);
-	postRequest.setRequestHeader('Content-Type', 'application/json');
+  console.log("== Request Body:", requestBody);
+  postRequest.setRequestHeader('Content-Type', 'application/json');
 
-	postRequest.addEventListener('load', function (event) {
-		console.log("== status:", event.target.status);
-		if(event.target.status !== 200){
-			var responseBody = event.target.response;
-			alert("Error deleting item on server side: ", + responseBody);
-		} else {
-			itemElem.remove();
-		}
-	});
-	postRequest.send(requestBody);
+  postRequest.addEventListener('load', function(event) {
+    console.log("== status:", event.target.status);
+    if (event.target.status !== 200) {
+      var responseBody = event.target.response;
+      alert("Error deleting item on server side: ", +responseBody);
+    } else {
+      itemElem.remove();
+    }
+  });
+  postRequest.send(requestBody);
 });
 
 
 $('section').on('click', '#bookmark', function() {
 
-	console.log("Class:", $(this).attr('class'));
+  console.log("Class:", $(this).attr('class'));
 
-	var itemElem = $(this).parent().parent().parent();
-	var caption = itemElem.attr('data-caption');
-	var postRequest = new XMLHttpRequest();
+  var itemElem = $(this).parent().parent().parent();
+  var caption = itemElem.attr('data-caption');
+  var postRequest = new XMLHttpRequest();
 
-	if($(this).attr('class')==="fa-star far"){
-		$(this).removeClass('far');
-		$(this).addClass('fas');
-		itemElem.attr('data-bookmark', true);
-		var requestURL = '/addBookmark';
-	}else{
-		$(this).removeClass('fas');
-		$(this).addClass('far');
-		itemElem.attr('data-bookmark', false);
-		var requestURL = '/deleteBookmark';
-	}
-	$(this).off('click');
+  if ($(this).attr('class') === "fa-star far") {
+    $(this).removeClass('far');
+    $(this).addClass('fas');
+    itemElem.attr('data-bookmark', true);
+    var requestURL = '/addBookmark';
+  } else {
+    $(this).removeClass('fas');
+    $(this).addClass('far');
+    itemElem.attr('data-bookmark', false);
+    var requestURL = '/deleteBookmark';
+  }
+  $(this).off('click');
 
-	postRequest.open('POST', requestURL);
+  postRequest.open('POST', requestURL);
 
-	var requestBody = JSON.stringify({
-		CAPTION: caption
-	});
+  var requestBody = JSON.stringify({
+    CAPTION: caption
+  });
 
-	console.log("== Request Body:", requestBody);
-	postRequest.setRequestHeader('Content-Type', 'application/json');
+  console.log("== Request Body:", requestBody);
+  postRequest.setRequestHeader('Content-Type', 'application/json');
 
-	postRequest.addEventListener('load', function (event) {
-		console.log("== status:", event.target.status);
-		if(event.target.status !== 200){
-			var responseBody = event.target.response;
-			alert("Error applying item on server side: ", + responseBody);
-		} else {
-			searchbox_filter();
-		}
-	});
-	postRequest.send(requestBody);
-	
+  postRequest.addEventListener('load', function(event) {
+    console.log("== status:", event.target.status);
+    if (event.target.status !== 200) {
+      var responseBody = event.target.response;
+      alert("Error applying item on server side: ", +responseBody);
+    } else {
+      searchbox_filter();
+    }
+  });
+  postRequest.send(requestBody);
+
 });
-
